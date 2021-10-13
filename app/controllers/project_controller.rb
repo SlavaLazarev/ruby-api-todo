968bce839ad71401649904964c06c749bc62e504
Vyacheslav <67823046+SlavaLazarev@users.noreply.github.com>
2021-10-13 21:27:12 +0700

    Update project_controller.rb

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -23,3 +23,17 @@ class ProjectController < ApplicationController
     render json: response
   end
 end
+def new     
+    if !params[:title].blank?
+      @title=params[:title]
+      if !Project.exists?(@title)
+        begin
+          Project.create(title: @title)          
+        rescue 
+          render json: {status: 500, msg: 'Server error'}
+        else
+          render json: {status: 201, msg: 'successfully'}
+        end
+      end
+    end
+  end
