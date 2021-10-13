a9295974cd7a283a11c154ee778698b896ed2b92
Vyacheslav <67823046+SlavaLazarev@users.noreply.github.com>
2021-10-13 23:57:16 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 #   get '/', to: redirect('projects')
   get "/", to: "project#index"
   get "projects", to: "project#index"
-  post "projects", to: "project#new"
+  get "new", to: "project#new"
   post "todos", to: "todo#new"
   patch "projects/:categoryid/todo/:todoid", to: "todo#edit"
 
