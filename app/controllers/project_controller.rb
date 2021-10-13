0661a260de893d58aa867b7d63e889219fff812c
Vyacheslav <67823046+SlavaLazarev@users.noreply.github.com>
2021-10-13 22:24:06 +0700

    Update project_controller.rb

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,37 +4,37 @@ class ProjectController < ApplicationController
     @Projects = Project.all
     @Todos = Todo.all
 
-    response = []
-    for project in @Projects
-      project_todos = []
-      for todo in @Todos.select {|todo| todo.categoryId == project.id}
-        project_todos.push({
-                             id: todo.id,
-                             text: todo.text,
-                             isCompleted: todo.isCompleted
-                           })
-      end
-      response.push({
-                 id: project.id,
-                 title: project.title,
-                 todos: project_todos
-               })
-    end
-    render json: response
-  end
-end
-def new     
-    if !params[:title].blank?
-      @title=params[:title]
-      if !Project.exists?(@title)
-        begin
-          Project.create(title: @title)          
-        rescue 
-          render json: {status: 500, msg: 'Server error'}
-        else
-          render json: {status: 201, msg: 'successfully'}
-        end
-      end
-    end
-  end
+#     response = []
+#     for project in @Projects
+#       project_todos = []
+#       for todo in @Todos.select {|todo| todo.categoryId == project.id}
+#         project_todos.push({
+#                              id: todo.id,
+#                              text: todo.text,
+#                              isCompleted: todo.isCompleted
+#                            })
+#       end
+#       response.push({
+#                  id: project.id,
+#                  title: project.title,
+#                  todos: project_todos
+#                })
+#     end
+#     render json: response
+#   end
+# end
+# def new     
+#     if !params[:title].blank?
+#       @title=params[:title]
+#       if !Project.exists?(@title)
+#         begin
+#           Project.create(title: @title)          
+#         rescue 
+#           render json: {status: 500, msg: 'Server error'}
+#         else
+#           render json: {status: 201, msg: 'successfully'}
+#         end
+#       end
+#     end
+#   end
 end
