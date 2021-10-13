64d336a3afc8e94c85ab389975f13e2d0bc89d43
Vyacheslav <67823046+SlavaLazarev@users.noreply.github.com>
2021-10-14 00:02:51 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,17 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ruby
+module Todolist
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :patch, :post, :options]
-      end
-    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
